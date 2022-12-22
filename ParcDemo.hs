9fb0bf14f3c88bacba06125952510e21a48f956c
Chris Pressey <cpressey@catseye.tc>
2022-12-22 09:56:45 +0000

    Demo backtracking behaviour

--- ParcDemo.hs
+++ ParcDemo.hs
@@ -19,5 +19,18 @@ bitstring = seq bits (try (char 'B'))
 docco = many (seq whitespaces bitstring)
 
 -- Demo
+
 test1 = docco $ Parsing "1110B   100B"
 test2 = docco $ Parsing "1110G   100B"
+
+-- Demo backtracking behaviour
+
+one = char '1'
+five = seq one (seq one (seq one (seq one one)))
+nine = seq one (seq one (seq one (seq one (seq one (seq one (seq one (seq one one)))))))
+multiple = many (alt nine five)
+
+testb1 = multiple $ Parsing "11111"
+testb2 = multiple $ Parsing "111111111"
+testb3 = multiple $ Parsing "1111111111111111111" -- 19, fail (9+9=18)
+testb4 = multiple $ Parsing "111111111111111111111" -- 21, fail
